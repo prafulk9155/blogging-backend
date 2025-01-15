b7f198e3f4e1150060f13737b37e4bb0bd26eace
praful kumar <prafulk9155@gmail.com>
2025-01-15 12:10:16 +0530

    new libraries and routes added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 
+
+// Configure CORS
+const corsOptions = {
+    origin: '*', // Allow only this domain
+    methods: ['GET', 'POST'],    // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+};
+
+// ---------------------------------Enable Libraries ---------------------------
+app.use(cors(corsOptions));
+app.use(morgan('combined'));
+
+//------------------------------------------------------------------------------
+
+//------------------------------- Middlewares -------------------------------
+
+
+
+//------------------------------- Public Routes -------------------------------
 app.get("/", (req, res) => res.send("Blogging Api on Vercel released on 15th jan 2025"));
 
+app.get("/api-test", (req, res) => {
+    res.json({
+        message: "Successfully connected with blogging api from Vercel"
+    });
+});
+
+
+//------------------------------- Private Routes close -------------------------------
+
+
+
 app.listen(3000, () => console.log("Server ready on port 3000..."));
+
+
 
 module.exports = app;
