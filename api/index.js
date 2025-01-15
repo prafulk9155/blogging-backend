8a75daac514ab73901177e55ef3dd1d3d9b067bc
praful kumar <prafulk9155@gmail.com>
2025-01-15 11:00:46 +0530

    index updated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Blogging Api on Vercel"));
+app.get("/", (req, res) => res.send("Blogging Api on Vercel released on 15th jan 2025"));
 
 app.listen(3000, () => console.log("Server ready on port 3000..."));
 
