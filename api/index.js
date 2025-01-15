47cf88c62ea8073d1d0864fcb4e8824726334380
praful kumar <prafulk9155@gmail.com>
2025-01-15 10:59:06 +0530

    index change for backend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Blogging Api on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server ready on port 3000..."));
 
 module.exports = app;
